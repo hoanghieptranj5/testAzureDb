458fd09d4656a8bbd2b40d9c3a6a81e6e0710977
Hiep Tran <hiep.tran@zoi.de>
2023-01-06 16:45:27 +0700

    removed credentials out of file

--- TestAzureDb/Model/ApplicationDbContext.cs
+++ TestAzureDb/Model/ApplicationDbContext.cs
@@ -36,8 +36,6 @@ namespace TestAzureDb.Model
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=tcp:mysqlserverhip.database.windows.net,1433;Initial Catalog=mySampleDatabse;Persist Security Info=False;User ID=azureuser;Password=Protoss5195;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
