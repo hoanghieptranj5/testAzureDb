d9e8865dcfd5d3413ac27afeddc776a417bcba94
Hiep Tran <hiep.tran@zoi.de>
2023-01-10 19:59:24 +0700

    removed credentials

--- Repositories/Model/ApplicationDbContext.cs
+++ Repositories/Model/ApplicationDbContext.cs
@@ -33,8 +33,7 @@ namespace Repositories.Model
         {
             if (!optionsBuilder.IsConfigured)
             {
-                string connectionString = "Server=tcp:mysqlserverhip.database.windows.net,1433;Initial Catalog=mySampleDatabse;Persist Security Info=False;User ID=azureuser;Password=Protoss5195;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-                optionsBuilder.UseSqlServer(connectionString);
+               
             }
         }
 
